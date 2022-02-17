17def63ec9cbc49c14662436cec23928d509aaa1
Marcel Piešťanský <marcel@piestansky.cz>
2022-02-17 10:31:44 +0100

    Initial commit

---
+++ TinyWeather/RootViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RootViewController.swift
+//  TinyWeather
+//
+//  Created by Marcel Piešťanský.
+//  Copyright © 2022 Marcel Piestansky. All rights reserved.
+//
+//  This program is free software. You can redistribute and/or modify it in
+//  accordance with the terms of the accompanying license agreement.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
