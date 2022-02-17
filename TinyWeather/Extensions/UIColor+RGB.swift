ec9e520683126fa191cbd1be6c34a3127e99a869
Marcel Piešťanský <marcel@piestansky.cz>
2022-02-17 12:22:20 +0100

    Added UIColor extension

---
+++ TinyWeather/Extensions/UIColor+RGB.swift
@@ -0,0 +1,47 @@
+//
+//  UIColor+RGB.swift
+//  TinyWeather
+//
+//  Created by Marcel Piešťanský.
+//  Copyright © 2022 Marcel Piestansky. All rights reserved.
+//  
+//  This program is free software. You can redistribute and/or modify it in
+//  accordance with the terms of the accompanying license agreement.
+//  
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    convenience init(red: Int, green: Int, blue: Int) {
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
+    }
+    
+    convenience init(red: Int, green: Int, blue: Int, a: Int = 0xFF) {
+        self.init(
+            red: CGFloat(red) / 255.0,
+            green: CGFloat(green) / 255.0,
+            blue: CGFloat(blue) / 255.0,
+            alpha: CGFloat(a) / 255.0
+        )
+    }
+    
+    convenience init(argb: Int) {
+        self.init(
+            red: (argb >> 16) & 0xFF,
+            green: (argb >> 8) & 0xFF,
+            blue: argb & 0xFF,
+            a: (argb >> 24) & 0xFF
+        )
+    }
+    
+}
