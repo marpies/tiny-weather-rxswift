a395b8c2e6746bff3e07f80d587f539d41a21e5d
Marcel Piešťanský <marcel@piestansky.cz>
2022-03-28 21:06:53 +0200

    Added "app route" enum

---
+++ TinyWeather/Scenes/Routes/RoutePanAnimation.swift
@@ -0,0 +1,19 @@
+//
+//  RoutePanAnimation.swift
+//  TinyWeather
+//
+//  Created by Marcel Piešťanský.
+//  Copyright © 2022 Marcel Piestansky. All rights reserved.
+//  
+//  This program is free software. You can redistribute and/or modify it in
+//  accordance with the terms of the accompanying license agreement.
+//  
+
+import Foundation
+import CoreGraphics
+
+enum RoutePanAnimation {
+    case began
+    case changed(translation: CGPoint)
+    case ended(velocity: CGPoint)
+}
