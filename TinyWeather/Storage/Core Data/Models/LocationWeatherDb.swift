6ee0da955c02162f1998872319e8ecda390f1463
Marcel Piešťanský <marcel@piestansky.cz>
2022-04-11 10:25:34 +0200

    Added "timezoneOffset", "isNight", "snowAmount" properties

--- TinyWeather/Storage/Core Data/Models/LocationWeatherDb.swift
+++ TinyWeather/Storage/Core Data/Models/LocationWeatherDb.swift
@@ -23,16 +23,19 @@ class LocationWeatherDb: NSManagedObject {
         let condition: Int
         let conditionDescription: String
         let lastUpdate: Date
+        let timezoneOffset: Double
         let sunrise: Date
         let sunset: Date
         let windSpeed: Float
         let rainAmount: Float
+        let snowAmount: Float
         let temperature: Float
         let name: String
         let country: String
         let state: String?
         let lon: Double
         let lat: Double
+        let isNight: Bool
         let daily: [DailyWeatherDb.Model]
     }
     
@@ -40,22 +43,25 @@ class LocationWeatherDb: NSManagedObject {
     @NSManaged var condition: Int
     @NSManaged var conditionDescription: String
     @NSManaged var lastUpdate: Date
+    @NSManaged var timezoneOffset: Double
     @NSManaged var sunrise: Date
     @NSManaged var sunset: Date
     @NSManaged var windSpeed: Float
     @NSManaged var rainAmount: Float
+    @NSManaged var snowAmount: Float
     @NSManaged var temperature: Float
     @NSManaged var name: String
     @NSManaged var country: String
     @NSManaged var state: String?
     @NSManaged var lon: Double
     @NSManaged var lat: Double
+    @NSManaged var isNight: Bool
     @NSManaged var daily: NSSet
     
     var model: LocationWeatherDb.Model {
         let set: Set<DailyWeatherDb>? = self.mutableSetValue(forKeyPath: #keyPath(LocationWeatherDb.daily)) as? Set<DailyWeatherDb>
         let daily: [DailyWeatherDb.Model] = set?.map({ $0.model }).sorted(by: { $0.date < $1.date }) ?? []
-        return LocationWeatherDb.Model(date: date, condition: condition, conditionDescription: conditionDescription, lastUpdate: lastUpdate, sunrise: sunrise, sunset: sunset, windSpeed: windSpeed, rainAmount: rainAmount, temperature: temperature, name: name, country: country, state: state, lon: lon, lat: lat, daily: daily)
+        return LocationWeatherDb.Model(date: date, condition: condition, conditionDescription: conditionDescription, lastUpdate: lastUpdate, timezoneOffset: timezoneOffset, sunrise: sunrise, sunset: sunset, windSpeed: windSpeed, rainAmount: rainAmount, snowAmount: snowAmount, temperature: temperature, name: name, country: country, state: state, lon: lon, lat: lat, isNight: isNight, daily: daily)
     }
     
 }
