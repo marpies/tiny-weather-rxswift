8cc8e901b9ad62bb0cc18ce87479766a697f78c6
Marcel Piešťanský <marcel@piestansky.cz>
2022-04-28 17:27:12 +0200

    Removed unnecessary usage of Single

--- TinyWeather/Scenes/Weather/Services/WeatherLoadingService.swift
+++ TinyWeather/Scenes/Weather/Services/WeatherLoadingService.swift
@@ -23,40 +23,27 @@ struct WeatherLoadingService: WeatherLoading {
     }
     
     func loadWeather(latitude: Double, longitude: Double) -> Single<Weather.Overview.Response> {
-        return Single.create { single in
-            // Load storage first, then API if no cache recent exists
-            let loader = self.storage.loadLocationWeather(latitude: latitude, longitude: longitude)
-                .filter({ (weather) in
-                    self.isCacheRecent(weather.current)
-                })
-                .asObservable()
-                .first()
-                .catchAndReturn(nil)
-                .flatMap({ (r: Weather.Overview.Response?) -> Single<Weather.Overview.Response> in
-                    // Use the recent cache if we have it
-                    if let response = r {
-                        return Single.just(response)
-                    }
-                    
-                    // Make an API call if needed
-                    return self.apiService
-                        .execute(request: APIResource.currentAndDaily(lat: latitude, lon: longitude))
-                        .map({ (response: HTTPResponse) in
-                            try response.map(to: Weather.Overview.Response.self)
-                        })
-                })
-            
-            let disposable = loader
-                .subscribe(onSuccess: { weather in
-                    single(.success(weather))
-                }, onFailure: { error in
-                    single(.failure(error))
-                })
-            
-            return Disposables.create {
-                disposable.dispose()
-            }
-        }
+        // Load storage first, then API if no cache recent exists
+        return self.storage.loadLocationWeather(latitude: latitude, longitude: longitude)
+            .filter({ (weather) in
+                self.isCacheRecent(weather.current)
+            })
+            .asObservable()
+            .first()
+            .catchAndReturn(nil)
+            .flatMap({ (r: Weather.Overview.Response?) -> Single<Weather.Overview.Response> in
+                // Use the recent cache if we have it
+                if let response = r {
+                    return Single.just(response)
+                }
+                
+                // Make an API call if needed
+                return self.apiService
+                    .execute(request: APIResource.currentAndDaily(lat: latitude, lon: longitude))
+                    .map({ (response: HTTPResponse) in
+                        try response.map(to: Weather.Overview.Response.self)
+                    })
+            })
     }
     
     //
